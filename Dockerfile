704eece4f792e0b90cdce226ce98012a9dba3777
Mohit <mohit18.official@gmail.com>
2023-06-19 20:24:11 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# define a base image
+FROM node
+
+# setup env variables
+ENV DB_USER=admin
+ENV DB_PASS=password
+
+# execute commands on container
+RUN mkdir -p /home/app
+
+# executes on host
+COPY . /home/app/
+
+# change working directory
+WORKDIR /home/app
+
+# execute commands on container
+CMD ["node", "server.js"]
+
+
+
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+      <body style='background:#323232;color:#fff;'>
+        <center>
+            <h1>Welcome to my Website.</h1>
+        </center>
+      </body>
+    `);
+});
+
+const PORT = 80;
+app.listen(PORT, () => {
+  console.log(`This app is listening on  http://localhost:${PORT}`);
+});
